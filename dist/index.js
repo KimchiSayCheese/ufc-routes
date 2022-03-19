1dfc83519828888f514549ff1f1bd4e98496ce31
Paul Chon <Paul.Chon16@Hotmail.com>
2022-03-19 14:19:40 -0400

    runtimeregen fix

--- dist/index.js
+++ dist/index.js
@@ -6,24 +6,37 @@ require("dotenv/config");
 
 var _path = _interopRequireDefault(require("path"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _client = require("@prisma/client");
 
-var app = (0, _express["default"])();
-var PORT = process.env.PORT || 4004;
-app.use(_express["default"].json());
-app.use(_express["default"].urlencoded({
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const app = (0, _express.default)();
+const PORT = process.env.PORT || 4004;
+const prisma = new _client.PrismaClient();
+app.use(_express.default.json());
+app.use(_express.default.urlencoded({
   extended: false
 }));
-app.use(_express["default"]["static"](_path["default"].join(__dirname, './../src/views')));
-app.use('/favicon.ico', _express["default"]["static"](_path["default"].join(__dirname, './../src/views/favicon.ico')));
-app.get('/', function (req, res) {
-  res.sendFile(_path["default"].join(__dirname, './../src/views/index.html'));
+app.use(_express.default.static(_path.default.join(__dirname, './../src/views')));
+app.use('/favicon.ico', _express.default.static(_path.default.join(__dirname, './../src/views/favicon.ico')));
+app.get('/', (req, res) => {
+  res.sendFile(_path.default.join(__dirname, './../src/views/index.html'));
 });
-app.get('/fighter/:name', function (req, res) {
-  var name = req.params.name;
-  res.send(name);
+app.get('/fighter/:name', async (req, res) => {
+  const {
+    name
+  } = req.params;
+  const fighterFindManyArg = {
+    where: {
+      name: {
+        contains: name
+      }
+    }
+  };
+  const data = await prisma.fighter.findMany(fighterFindManyArg);
+  res.send(data);
 });
-app.listen(4000, function () {
-  console.log("listening in on port: ".concat(PORT));
+app.listen(process.env.PORT, () => {
+  console.log(`listening in on port: ${process.env.PORT}`);
 });
 //# sourceMappingURL=index.js.map
--- test.js
+++
@@ -1,4 +0,0 @@
-const path = require('path')
-
-console.log(path.resolve(__dirname, '/views'))
-console.log(path.join(__dirname, '/views'))
