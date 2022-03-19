f7eb7dd30bef28c48cfc9b76c2b5a4a6ca30a97a
Paul Chon <Paul.Chon16@Hotmail.com>
2022-03-19 13:56:05 -0400

    added a get end point

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express'
 import 'dotenv/config'
 import path from 'path'
+import { PrismaClient, Prisma } from '@prisma/client'
 
 const app = express()
 const PORT: any = process.env.PORT || 4004
+const prisma = new PrismaClient()
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -13,14 +16,28 @@ app.use(
   express.static(path.join(__dirname, './../src/views/favicon.ico'))
 )
 
+
+// Home Page (Not the focus of this project.)
+
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, './../src/views/index.html'))
-})
+})ß
 
-app.get('/fighter/:name', (req: Request, res: Response) => {
+
+
+// CRUD Operations
+
+app.get('/fighter/:name', async (req: Request, res: Response) => {
   const { name } = req.params
-
-  res.send(name)
+  const fighterFindManyArg: Prisma.FighterFindManyArgs = {
+    where: {
+      name: {
+        contains: name
+      }
+    }
+  }
+  const data = await prisma.fighter.findMany(fighterFindManyArg)
+  res.send(data)
 })
 
 app.listen(process.env.PORT, () => {
