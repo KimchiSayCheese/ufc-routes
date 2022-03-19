de420aec1e5735ae5dd4472def73e9a3d55ea9dd
Paul Chon <Paul.Chon16@Hotmail.com>
2022-03-19 02:43:35 -0400

    heroku is being messy

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import 'dotenv/config'
 import path from 'path'
 
 const app = express()
-const PORT: any = process.env.PORT || 4000
+const PORT: any = process.env.PORT || 4004
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, './../src/views')))
+app.use(
+  '/favicon.ico',
+  express.static(path.join(__dirname, './../src/views/favicon.ico'))
+)
 
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, './../src/views/index.html'))
@@ -19,6 +23,6 @@ app.get('/fighter/:name', (req: Request, res: Response) => {
   res.send(name)
 })
 
-app.listen(PORT, () => {
-  console.log(`listening in on port: ${PORT}`)
+app.listen(process.env.PORT, () => {
+  console.log(`listening in on port: ${process.env.PORT}`)
 })
